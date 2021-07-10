99ec3e5a72ae97934800f58345013df24c4b1a76
VBel3235 <Vlad95.0753@gmain.com>
2021-07-10 21:16:12 +1000

    Initial Commit

---
+++ M11LoginPasswordProjectPlusTests/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M11LoginPasswordProjectPlusTests
+//
+//  Created by Владислав Белов on 10.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
